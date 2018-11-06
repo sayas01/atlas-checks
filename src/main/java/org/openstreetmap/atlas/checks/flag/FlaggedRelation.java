57cde0f8da2e4445c6be84288943d61589c2e2c7
sayana-saithu <sayana_saithu@apple.com>
2018-11-05 16:02:47 -0800

    Return null for getGeometry of FlaggedRelations

--- src/main/java/org/openstreetmap/atlas/checks/flag/FlaggedRelation.java
+++ src/main/java/org/openstreetmap/atlas/checks/flag/FlaggedRelation.java
@@ -9,15 +9,9 @@ import java.util.Map;
 import java.util.Set;
 
 import org.openstreetmap.atlas.geography.Location;
-import org.openstreetmap.atlas.geography.PolyLine;
-import org.openstreetmap.atlas.geography.atlas.items.Area;
-import org.openstreetmap.atlas.geography.atlas.items.AtlasItem;
-import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
-import org.openstreetmap.atlas.geography.atlas.items.LocationItem;
 import org.openstreetmap.atlas.geography.atlas.items.Relation;
 import org.openstreetmap.atlas.geography.atlas.items.RelationMember;
 import org.openstreetmap.atlas.geography.atlas.items.RelationMemberList;
-import org.openstreetmap.atlas.utilities.collections.Iterables;
 
 /**
  * A flag for a {@link Relation}
@@ -36,50 +30,14 @@ public class FlaggedRelation extends FlaggedObject
     }
 
     /**
-     * Get locations of all flattened members
+     * As relations itself do not have a geometry, the geometry of FlaggedRelation is set to null.
      *
      * @return flagged geometry
      */
     @Override
     public Iterable<Location> getGeometry()
     {
-        final List<Location> listOfLocation = new ArrayList<>();
-
-        final Set<AtlasObject> flattenedMembers = this.relation.flatten();
-        if (flattenedMembers.size() == 1)
-        {
-            final AtlasObject object = flattenedMembers.iterator().next();
-            if (object instanceof LocationItem)
-            {
-                return ((LocationItem) object).getLocation();
-            }
-        }
-        for (final AtlasObject member : flattenedMembers)
-        {
-            if (member instanceof LocationItem)
-            {
-                listOfLocation.add(((LocationItem) member).getLocation());
-            }
-            else
-            {
-                if (member instanceof Area)
-                {
-                    // An Area's geometry doesn't include the end (same as start) location. To close
-                    // the
-                    // area boundary, we need to add the end location manually.
-                    final List<Location> geometry = Iterables
-                            .asList(((AtlasItem) member).getRawGeometry());
-                    geometry.add(geometry.get(0));
-                    listOfLocation.addAll(new PolyLine(geometry));
-                }
-                else
-                {
-                    listOfLocation.addAll(new PolyLine(((AtlasItem) member).getRawGeometry()));
-                }
-            }
-        }
-        final PolyLine polyLine = new PolyLine(listOfLocation);
-        return polyLine;
+        return null;
     }
 
     /**
